d3a5e49ac7fbfef02f659a2c718095f739f1aba8
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-06-22 22:24:45 -0300

    feat: filter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import Filter from "./components/Filter/Filter"
 import Home from "./components/Home/Home"
 import Cart from "./components/Cart/Cart"
 import { AppContainer } from "./style"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function App() {
   const [cart, setCart] = useState ([]);
+  const [ordination, setOrdination] = useState ("growing");
+  const [valueMax, setValueMax] = useState(0);
+  const [valueMin, setValueMin] = useState(0);
+  const [searchName, setSearchName] = useState("");
+  
+  useEffect(() => {
+    if (cart.length > 0){
+      localStorage.setItem("cart", JSON.stringify(cart))
+    } 
+  }, [cart]);
+
+  useEffect(() => {
+    const cartLocalStorage = localStorage.getItem("cart")
+    if(cartLocalStorage){
+      setCart(JSON.parse(cartLocalStorage));
+    }
+  }, [])
 
   return (
     <AppContainer>
-      <Filter> </Filter>
+      <Filter ordination={ordination} setOrdination={setOrdination}
+      setValueMax={setValueMax} setValueMin={setValueMin}
+      setSearchName={setSearchName}> </Filter>
       <Home cart={cart} setCart={setCart}> </Home>
       <Cart cart={cart} setCart={setCart}> </Cart>
     </AppContainer>
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,5 +1,20 @@
-const Filter = () => {
-    return <div> Filter </div>;
+import { InputContainer } from "./style";
+
+const Filter = ({ordination, setOrdination, setValueMax, setValueMin, setSearchName}) => {
+    return (
+        <div>
+            <p>Filtro</p>
+            <select value={ordination} onChange={(e) => setOrdination(e.target.value)}>
+                <option value="growing">Crescente</option>
+                <option value="descending">Decrescente</option>
+            </select>
+            <InputContainer>
+                <input placeholder="Valor mínimo" type="number" onChange={(e) => setValueMin(e.target.value)}/>
+                <input placeholder="Valor máximo" type="number" onChange={(e) => setValueMax(e.target.value)}/>
+                <input placeholder="Busca por nome" type="text" onChange={(e) => setSearchName (e.target.value)}/>
+            </InputContainer>
+        </div>
+    );
 }
 
 export default Filter;
